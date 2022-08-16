820a8862c86c07150de7bdfc7fd5f64f6e803286
chlpaegi01 <chlpaegi01@netand.co.kr>
2022-08-16 17:49:08 +0900

    2022-08-16 17:48
    동적계획법
    - 도둑질

---
+++ day4/src/day4.java
@@ -0,0 +1,16 @@
+public class day4 {
+
+    public static void main(String[] args) {
+        int[] money = {1,2,3,4};
+        Solution.solution(money);
+    }
+}
+
+class Solution {
+    public static int solution(int[] money) {
+        int answer = 0;
+
+        return answer;
+    }
+}
+
