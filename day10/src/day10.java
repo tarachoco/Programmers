301123a2da7f6a00c11f7f97ced93b443c877c62
chlpaegi01 <chlpaegi01@netand.co.kr>
2022-08-24 14:33:40 +0900

    2022-08-24 14:33 DFS 타겟넘버

--- day10/src/day10.java
+++ day10/src/day10.java
@@ -1,31 +1,26 @@
 public class day10 {
     public static void main(String[] args) {
 //         1, 2, 1, 2 / 6 / 1
-        int[] s = {1, 2, 1, 2};
-//        int[] s = {1,1,1,1,1};
+//        int[] s = {1, 2, 1, 2};
+        int[] s = {1,1,1,1,1};
         Solution so = new Solution();
-        so.solution(s, 6);
+        so.solution(s, 3);
     }
 }
 
 class Solution {
     public int solution(int[] numbers, int target) {
-        int answer = 0;
-        answer+= dfs(numbers[0],0, 1,numbers,target);
-        answer+= dfs(-numbers[0],0, 1,numbers,target);
-        System.out.printf("result="+answer);
+        int answer = dfs(numbers, 0, 0, target);
         return answer;
     }
 
-    public int dfs (int k, int r, int idx, int[] numbers, int target){
-        if(idx >= numbers.length) {
-            System.out.printf("k="+k+", r="+r+"\n");
-            if(r==target) {
+    public int dfs (int[] numbers, int sum, int idx, int target){
+        if(idx == numbers.length){
+            if(sum == target){
                 return 1;
             }
             return 0;
         }
-        int n = numbers[idx];
-        return dfs(n,r+k, ++idx, numbers, target ) + dfs(n,r-k, ++idx, numbers, target);
+        return dfs(numbers, sum+numbers[idx], idx+1, target) + dfs(numbers, sum-numbers[idx], idx+1, target);
     }
 }
