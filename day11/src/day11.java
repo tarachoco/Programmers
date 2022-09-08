eb6d4559f0735157d7f105b0323fb5c2fa4b355e
chlpaegi01 <chlpaegi01@netand.co.kr>
2022-09-08 10:22:42 +0900

    2022-09-08 10:22 초기화

--- day11/src/day11.java
+++ day11/src/day11.java
@@ -1,5 +1,13 @@
 public class day11 {
     public static void main(String[] args) {
-
+        Solution so = new Solution();
+        int[][] arr = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
+        so.solution(arr);
+    }
+}
+class Solution {
+    public int[] solution(int n, int[] info) {
+        int[] answer = {};
+        return answer;
     }
 }
