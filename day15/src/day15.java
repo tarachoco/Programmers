cbc02364b7ce1382aa45a8335ec6c62142e5b0ad
chlpaegi01 <chlpaegi01@netand.co.kr>
2023-09-12 15:44:16 +0900

    2023-09-12 15:44
    - 체육복

---
+++ day15/src/day15.java
@@ -0,0 +1,10 @@
+public class day15 {
+    /*
+        탐욕법
+        - #42862 체육복
+        https://school.programmers.co.kr/learn/courses/30/lessons/42862
+     */
+    public static void main(String[] args) {
+
+    }
+}
