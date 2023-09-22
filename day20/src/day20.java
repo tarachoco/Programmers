6302db37da6c7b092f2a2d91e1d07d066b5ad819
chlpaegi01 <chlpaegi01@naver.com>
2023-09-22 21:27:18 +0900

    2023-09-22 21:26
    - StringBuilder로 메모리 아끼기

--- day20/src/day20.java
+++ day20/src/day20.java
@@ -1,5 +1,30 @@
 public class day20 {
+    /*
+        JadenCase 문자열 만들기
+        https://school.programmers.co.kr/learn/courses/30/lessons/12951
+     */
     public static void main(String[] args) {
-        
+        String result = solution("3people unFollowed me ");
+        System.out.println(result);
+    }
+
+    public static String solution(String s) {
+        if (s.length() == 0) return s;
+
+        int i = 1;
+
+        StringBuilder sb = new StringBuilder(s.toLowerCase());
+        sb.replace(0,1, sb.substring(0,1).toUpperCase());
+
+        while (i<s.length()) {
+            int start = s.indexOf(" ",i)+1;
+            int end = start+1;
+
+            if ( start < 0 || end >= s.length() ) break;
+            sb.replace(start,end, sb.substring(start,end).toUpperCase());
+            i += end - start;
+        }
+
+        return sb.toString();
     }
 }
