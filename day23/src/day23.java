e597756a086fce02c86db4d5f252c05e2a7b16b8
chlpaegi01 <chlpaegi01@naver.com>
2023-09-27 15:30:00 +0900

    2023-09-27 15:29
    - K번째수

--- day23/src/day23.java
+++ day23/src/day23.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+
 public class day23 {
+    /*
+        K번째수
+        https://school.programmers.co.kr/learn/courses/30/lessons/42748
+     */
     public static void main(String[] args) {
-        
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        Arrays.stream(solution(array, commands)).forEach(System.out::println);
+    }
+
+    public static int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i=0; i<commands.length; i++) {
+            int[] arr = Arrays.copyOfRange(array,commands[i][0]-1, commands[i][1]);
+            Arrays.sort(arr);
+            answer[i] = arr[commands[i][2]-1];
+        }
+
+        return answer;
     }
 }
