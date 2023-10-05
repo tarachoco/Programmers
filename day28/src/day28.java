eb5b3a162d9d273194e712ef8d324232ac774511
chlpaegi01 <chlpaegi01@naver.com>
2023-10-05 23:17:41 +0900

    2023-10-05 23:17
    - 가장 가까운 글자

--- day28/src/day28.java
+++ day28/src/day28.java
@@ -12,19 +12,15 @@ public class day28 {
     public static int[] solution(String s) {
         int[] answer = new int[s.length()];
 
-        for (int i=1; i<s.length(); i++) {
-            System.out.println(s.substring(0,i));
-            System.out.println(s.charAt(i-1));
+        for (int i=1; i<=s.length(); i++) {
 
-            int idx = s.substring(0,i-1).indexOf(s.charAt(i-1));
+            int idx = s.substring(0,i-1).lastIndexOf(s.charAt(i-1));
             if (idx > -1) {
-                answer[i-1] = i - idx;
+                answer[i-1] = i - idx -  1;
             } else {
                 answer[i-1] = -1;
             }
         }
-
-        System.out.println("-----------");
 
         return answer;
     }
