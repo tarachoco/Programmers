6ab6fc52fdb25681d3094a212f2a3398cc3d1330
chlpaegi01 <chlpaegi01@naver.com>
2023-10-13 19:30:14 +0900

    2023-10-13 19:30
    - 완주하지 못한 선수

--- day33/src/day33.java
+++ day33/src/day33.java
@@ -1,5 +1,30 @@
+import java.util.*;
+
 public class day33 {
+    /*
+        완주하지 못한 선수
+        https://school.programmers.co.kr/learn/courses/30/lessons/42576/solution_groups?language=java
+     */
     public static void main(String[] args) {
-        
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+
+        System.out.println(solution(participant, completion));
+    }
+
+    public static String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for (int i=0; i<participant.length; i++) {
+            if (completion.length == i || !participant[i].equals(completion[i])) {
+                answer = participant[i];
+                break;
+            }
+        }
+
+        return answer;
     }
 }
