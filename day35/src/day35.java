279fcccb47a58a49d3cdbc79bf4d3a87cf5a04a0
chlpaegi01 <chlpaegi01@naver.com>
2023-10-18 00:08:13 +0900

    2023-10-17 24:00
    - 우선순위 큐 기억할 것

--- day35/src/day35.java
+++ day35/src/day35.java
@@ -1,5 +1,32 @@
+import java.util.PriorityQueue;
+
 public class day35 {
+    /*
+        더 맵게
+        https://school.programmers.co.kr/learn/courses/30/lessons/42626
+     */
     public static void main(String[] args) {
-        
+        int[] scoville = {1, 2, 3, 9, 10, 12};
+        System.out.println(solution(scoville, 7));
+    }
+
+    public static int solution(int[] scoville, int K) {
+        int answer = 0;
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for (int s : scoville) {
+            queue.add(s);
+        }
+
+        while (queue.peek() < K) {
+            if (queue.size() == 1) {
+                return -1;
+            }
+            queue.add(queue.poll() + (queue.poll() * 2));
+            answer++;
+        }
+
+        return answer;
     }
 }
