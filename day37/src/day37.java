32096d54a460ed5db6c94bababbf04fd482ddff2
chlpaegi01 <chlpaegi01@naver.com>
2023-10-21 20:03:27 +0900

    2023-10-21 20:03
    - day37

--- day37/src/day37.java
+++ day37/src/day37.java
@@ -1,5 +1,43 @@
-public class day37 {
-    public static void main(String[] args) {
+    import java.util.LinkedList;
+    import java.util.Queue;
 
+public class day37 {
+    /*
+        다리를 건너는 트럭
+        1초에 한 칸씩
+        https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=java
+     */
+    public static void main(String[] args) {
+        int[] truck_weights = {7,4,5,6};
+        System.out.println(solution(2, 10, truck_weights));
+    }
+
+    public static int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+
+        Queue<Integer> queue = new LinkedList<>();
+        int i = 0;
+        int bridge_weight = 0;
+
+        while (i < truck_weights.length) {
+            int truck = truck_weights[i];
+
+            if (queue.size() == bridge_length) {
+                // 다리를 다 건넜다
+                bridge_weight-= queue.poll();
+            }
+
+            if (bridge_weight + truck <= weight) {
+                queue.add(truck);
+                bridge_weight+=truck;
+                i++;
+            } else {
+                queue.add(0);
+            }
+
+            answer++;
+        }
+
+        return answer + bridge_length;
     }
 }
