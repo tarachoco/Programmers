159515696f4588ced403c5a4f4fbe056f5c5709c
chlpaegi01 <chlpaegi01@naver.com>
2023-10-24 22:42:15 +0900

    2023-10-24 10:42
    - 문자열 나누기

--- day39/src/day39.java
+++ day39/src/day39.java
@@ -4,6 +4,32 @@ public class day39 {
         https://school.programmers.co.kr/learn/courses/30/lessons/140108
      */
     public static void main(String[] args) {
+        System.out.println(solution("banana"));
+    }
 
+    public static int solution(String s) {
+        int answer = 0;
+
+        while (s.length() > 0) {
+            answer++;
+
+            char t = s.charAt(0);
+            int a = 1;
+            int b = 0;
+            for (int i=1; i<s.length(); i++) {
+                if (t == s.charAt(i)) {
+                    a++;
+                } else {
+                    b++;
+                }
+
+                if (a==b) {
+                    break;
+                }
+            }
+            s = s.substring(a+b);
+        }
+
+        return answer;
     }
 }
