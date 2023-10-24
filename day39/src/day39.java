3464814b868e7f6dcad0e9565adcc1698bff8dc4
chlpaegi01 <chlpaegi01@naver.com>
2023-10-24 20:43:56 +0900

    2023-10-24 20:43
    - 문자열 나누기

--- day39/src/day39.java
+++ day39/src/day39.java
@@ -1,5 +1,9 @@
 public class day39 {
+    /*
+        문자열 나누기
+        https://school.programmers.co.kr/learn/courses/30/lessons/140108
+     */
     public static void main(String[] args) {
-        
+
     }
 }
