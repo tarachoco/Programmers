0030206181a51d2c20bbd56f42818ecdc766dd3d
chlpaegi01 <chlpaegi01@naver.com>
2023-10-26 20:26:25 +0900

    2023-10-26 20:26
    - 이진변환 반복하기

--- day40/src/day40.java
+++ day40/src/day40.java
@@ -1,5 +1,32 @@
+import java.util.Arrays;
+
 public class day40 {
+    /*
+        이진변환 반복하기
+        https://school.programmers.co.kr/learn/courses/30/lessons/70129
+     */
     public static void main(String[] args) {
-        
+        int[] results = solution("011010");
+        Arrays.stream(results).forEach(System.out::println);
+    }
+
+    public static int[] solution(String s) {
+        int[] answer = new int[2];
+
+        while (s.length() > 1) {
+
+            int cnt = 0;
+            for (int i=0; i<s.length(); i++) {
+                if (s.charAt(i) == '0') {
+                    answer[1]++;
+                } else {
+                    cnt++;
+                }
+            }
+            s = Integer.toBinaryString(cnt);
+            answer[0]++;
+        }
+
+        return answer;
     }
 }
