41a4c6501705cde902515efeaf899c7c2b699982
chlpaegi01 <chlpaegi01@naver.com>
2023-10-28 17:04:23 +0900

    2023-10-28 17:04
    - 다음 큰 숫자

--- day41/src/day41.java
+++ day41/src/day41.java
@@ -1,5 +1,24 @@
 public class day41 {
+    /*
+        다음 큰 숫자
+        https://school.programmers.co.kr/learn/courses/30/lessons/12911
+     */
     public static void main(String[] args) {
-        
+        System.out.println(solution(78));
+    }
+
+    public static int solution(int n) {
+        int answer = n;
+        int bitCount = Integer.bitCount(n);
+
+        while (true) {
+            answer++;
+
+            if (bitCount == Integer.bitCount(answer)) {
+                break;
+            }
+        }
+
+        return answer;
     }
 }
