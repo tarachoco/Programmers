06698b927ae174e3131cc43eff668f54becdb7b6
chlpaegi01 <chlpaegi01@naver.com>
2023-11-03 19:52:29 +0900

    2023-11-03 19:52
    - 롤케이크 자르기

--- day44/src/day44.java
+++ day44/src/day44.java
@@ -1,5 +1,17 @@
 public class day44 {
+    /*
+        롤케이크 자르기
+        https://school.programmers.co.kr/learn/courses/30/lessons/132265/questions?page=3
+     */
     public static void main(String[] args) {
-        
+
+    }
+
+    public int solution(int[] topping) {
+        int answer = -1;
+        for (int i=1; i<topping.length-1; i++) {
+
+        }
+        return answer;
     }
 }
