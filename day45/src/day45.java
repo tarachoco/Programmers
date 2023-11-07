68a919eaa6cd32beb706d1613fd7fa30305577bc
chlpaegi01 <chlpaegi01@naver.com>
2023-11-07 21:31:26 +0900

    2023-11-07 21:31
    - 점프와 순간이동

--- day45/src/day45.java
+++ day45/src/day45.java
@@ -1,5 +1,9 @@
 public class day45 {
+    /*
+        점프와 순간이동
+        https://school.programmers.co.kr/learn/courses/30/lessons/12980
+     */
     public static void main(String[] args) {
-        
+
     }
 }
