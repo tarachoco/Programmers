1baaa554ab7535dd4a3ffc7bf994d28bfa84dcdd
chlpaegi01 <chlpaegi01@naver.com>
2023-11-17 21:43:42 +0900

    2023-11-17 21:43
    - 공원 산책

--- day48/src/day48.java
+++ day48/src/day48.java
@@ -1,5 +1,9 @@
 public class day48 {
+    /*
+        공원산책
+        https://school.programmers.co.kr/learn/courses/30/lessons/172928
+     */
     public static void main(String[] args) {
-        
+
     }
 }
