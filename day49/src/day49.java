801c48df7bf0ea5071f7ccc9254040775233e95f
chlpaegi01 <chlpaegi01@naver.com>
2023-11-21 21:09:14 +0900

    2023-11-21 21:09
    - day49

--- day49/src/day49.java
+++ day49/src/day49.java
@@ -1,5 +1,9 @@
 public class day49 {
+    /*
+        신규 아이디 추천
+        https://school.programmers.co.kr/learn/courses/30/lessons/72410
+     */
     public static void main(String[] args) {
-        
+
     }
 }
