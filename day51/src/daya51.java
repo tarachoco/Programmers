8e68da357167aee0687d4759fefda398d6b5dd5a
chlpaegi01 <chlpaegi01@naver.com>
2023-11-26 18:45:01 +0900

    2023-11-26 18:44
    - day51

--- day51/src/daya51.java
+++ day51/src/daya51.java
@@ -1,5 +1,9 @@
 public class daya51 {
+    /*
+        프로세스
+        https://school.programmers.co.kr/learn/courses/30/lessons/42587
+     */
     public static void main(String[] args) {
-        
+
     }
 }
