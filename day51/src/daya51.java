bba9f8dc22fa325c2d27c7cb8e252a92301020dc
chlpaegi01 <chlpaegi01@naver.com>
2023-11-27 17:38:02 +0900

    2023-11-27 17:36
    - 프로세스
    - 우선순위를 배열로 비교하는 방법 고려할 것

--- day51/src/daya51.java
+++ day51/src/daya51.java
@@ -8,13 +8,13 @@ public class daya51 {
         https://school.programmers.co.kr/learn/courses/30/lessons/42587
      */
     public static void main(String[] args) {
-        int[] priorities = {2, 1, 3, 2};
+//        int[] priorities = {1, 1, 9, 1, 1, 1};
         int location = 2;
+        int[] priorities = {2,1,3,2};
         System.out.println(solution(priorities, location));
     }
 
     public static int solution(int[] priorities, int location) {
-        int answer = 0;
         Queue<Integer> queue = new LinkedList<>();
         for (int i=0; i<priorities.length; i++) {
             queue.add(priorities[i]);
@@ -24,22 +24,25 @@ public class daya51 {
         int l=0;
 
         while (true) {
-            if (i==location){
-                break;
-            }
+
             int poll = queue.poll();
+
             if (queue.stream().filter(q->q > poll).findAny().orElse(0) > 0) {
                 queue.offer(poll);
-                i++;
             } else {
                 queue.offer(0);
+                i++;
+                if (l==location) {
+                    return i;
+                }
             }
+
             l++;
+
             if (l>=priorities.length) {
                 l=0;
             }
-        }
 
-        return l;
+        }
     }
 }
