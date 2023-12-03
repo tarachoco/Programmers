38c4eaab39f44b86e9f158f1a4f899e066204c37
chlpaegi01 <chlpaegi01@naver.com>
2023-12-03 16:40:48 +0900

    2023-12-03 16:40
    - 연속 부분 수열 합의 개수

--- day53/src/day53.java
+++ day53/src/day53.java
@@ -1,5 +1,9 @@
 public class day53 {
+    /*
+        연속 부분 수열 합의 개수
+        https://school.programmers.co.kr/learn/courses/30/lessons/131701
+     */
     public static void main(String[] args) {
-        
+
     }
 }
