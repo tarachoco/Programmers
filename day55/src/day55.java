59a48b8c07512c5390c71fecd069d25d0335c071
chlpaegi01 <chlpaegi01@naver.com>
2023-12-09 21:26:23 +0900

    2023-12-09 21:26
    - day55

--- day55/src/day55.java
+++ day55/src/day55.java
@@ -4,6 +4,11 @@ public class day55 {
         https://school.programmers.co.kr/learn/courses/30/lessons/76502
      */
     public static void main(String[] args) {
+        System.out.println(solution("[](){}"));
+    }
 
+    public static int solution(String s) {
+        int answer = -1;
+        return answer;
     }
 }
