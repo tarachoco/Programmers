f2a406b64faeb0313bea0f7e6bf5a4d0722ea4a8
chlpaegi01 <chlpaegi01@naver.com>
2023-12-10 21:28:04 +0900

    2023-12-10 21:28
    - 괄호 회전하기

--- day55/src/day55.java
+++ day55/src/day55.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class day55 {
     /*
         괄호 회전하기
@@ -8,7 +10,33 @@ public class day55 {
     }
 
     public static int solution(String s) {
-        int answer = -1;
+        int answer = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            Stack<Character> stack = new Stack<>();
+
+            // 괄호 회전
+            String str = s.substring(i, s.length()) + s.substring(0, i);
+
+            for (int j = 0; j < str.length(); j++) {
+                char c = str.charAt(j);
+                if (stack.isEmpty()) {
+                    stack.push(c);
+                } else if (c == ')' && stack.peek() == '(') {
+                    stack.pop();
+                } else if (c == '}' && stack.peek() == '{') {
+                    stack.pop();
+                } else if (c == ']' && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+            if (stack.isEmpty()) {
+                answer++;
+            }
+        }
+
         return answer;
     }
 }
