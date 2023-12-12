5ac8edca201aa6abaa7aae94aef83e531d727d77
chlpaegi01 <chlpaegi01@naver.com>
2023-12-12 21:03:20 +0900

    2023-12-12 21:03
    - day56

--- day56/src/day56.java
+++ day56/src/day56.java
@@ -1,5 +1,9 @@
 public class day56 {
+    /*
+        피로도
+        https://school.programmers.co.kr/learn/courses/30/lessons/87946
+     */
     public static void main(String[] args) {
-        
+
     }
 }
