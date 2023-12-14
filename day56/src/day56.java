c6c9858d335b4f3f5cf96d42c5632c66e0d4e86c
chlpaegi01 <chlpaegi01@naver.com>
2023-12-14 17:16:36 +0900

    2023-12-14 17:16
    - 피로도 풀이 중

--- day56/src/day56.java
+++ day56/src/day56.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class day56 {
     /*
         피로도
@@ -5,5 +8,27 @@ public class day56 {
      */
     public static void main(String[] args) {
 
+    }
+
+    private static int answer = 0;
+
+    public static int solution(int k, int[][] dungeons) {
+        Set<Integer> remains = new HashSet<>();
+
+        for (int i=0; i<dungeons.length; i++) {
+            remains.add(i);
+        }
+
+        for (int i=0; i<dungeons.length; i++) {
+            dfs(k-dungeons[i][1], dungeons, remains);
+        }
+
+        return answer;
+    }
+
+    public static void dfs (int k, int[][] dungeons, Set<Integer> remains) {
+        for (int i=0; i<remains.size(); i++) {
+            dfs(k, dungeons, remains);
+        }
     }
 }
