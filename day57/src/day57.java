4f374cbaee275d0f8f5025c49e18b1633e0f564f
chlpaegi01 <chlpaegi01@naver.com>
2023-12-20 21:22:24 +0900

    2023-12-20 21:22
    - 전력망

--- day57/src/day57.java
+++ day57/src/day57.java
@@ -1,4 +1,8 @@
 public class day57 {
+    /*
+        전력망을 둘로 나누기
+        https://school.programmers.co.kr/learn/courses/30/lessons/86971
+     */
     public static void main(String[] args) {
         
     }
