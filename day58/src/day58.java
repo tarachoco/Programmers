3bfa4d467147f25839dd3a4a82c6f40645677c48
chlpaegi01 <chlpaegi01@naver.com>
2023-12-25 21:01:40 +0900

    2023-12-25 21:01
    - day58

--- day58/src/day58.java
+++ day58/src/day58.java
@@ -1,5 +1,9 @@
 public class day58 {
+    /*
+        카펫
+        https://school.programmers.co.kr/learn/courses/30/lessons/42842
+     */
     public static void main(String[] args) {
-        
+
     }
 }
