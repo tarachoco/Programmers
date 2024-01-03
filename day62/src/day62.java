cd9360d4521e2c687f588837af49c8fdc52eb9bc
chlpaegi01 <chlpaegi01@naver.com>
2024-01-03 13:59:21 +0900

    2023-01-03 13:59
    - 인사고과

--- day62/src/day62.java
+++ day62/src/day62.java
@@ -1,5 +1,9 @@
 public class day62 {
+    /*
+        인사고과
+        https://school.programmers.co.kr/learn/courses/30/lessons/152995
+     */
     public static void main(String[] args) {
-        
+
     }
 }
