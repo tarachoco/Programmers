da6e80695a44a66ae7fdaa0773ab3add883c6084
chlpaegi01 <chlpaegi01@naver.com>
2024-01-04 20:41:13 +0900

    2024-01-04 20:41
    - 인사고과

--- day62/src/day62.java
+++ day62/src/day62.java
@@ -6,4 +6,9 @@ public class day62 {
     public static void main(String[] args) {
 
     }
+
+    public static int solution(int[][] scores) {
+        int answer = 0;
+        return answer;
+    }
 }
