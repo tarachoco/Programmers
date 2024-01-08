39b10c6f7b8daa166e038a87e0f4dd69bc32bc6c
chlpaegi01 <chlpaegi01@naver.com>
2024-01-09 08:46:38 +0900

    2024-01-09 08:46
    - 게임 맵 최단거리

--- day63/src/day63.java
+++ day63/src/day63.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class day63 {
+public class day63 {
+    /*
+        게임 맵 최단거리
+        https://school.programmers.co.kr/learn/courses/30/lessons/1844
+     */
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int[][] maps) {
+        int answer = 0;
+        return answer;
+    }
 }
