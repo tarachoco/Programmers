b0fde52c9a112400be0726a4664f6e73f032fef9
chlpaegi01 <chlpaegi01@naver.com>
2024-01-12 09:50:02 +0900

    2024-01-12 09:49
    - 불필요 주석 삭제

--- day63/src/day63.java
+++ day63/src/day63.java
@@ -155,41 +155,4 @@ public class day63 {
             return y;
         }
     }
-
-    /*
-
-     public static int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
-
-        boolean[][] visited = new boolean[n][m];
-        Queue<int[]> queue = new LinkedList<>();
-        visited[0][0] = true;
-        queue.add(new int[] {0,0,1}); //{현재 위치, 현재위치, 총 수}
-
-        int[] dx = {-1,1,0,0};
-        int[] dy = {0,0,1,-1};
-        int answer = 0;
-
-        while(queue.isEmpty() == false){
-            int[] currPoint = queue.remove();
-            if(currPoint[0] == n-1 && currPoint[1] == m-1){
-                return currPoint[2];
-            }
-            for (int i = 0; i < 4; i++) {
-                int xx = dx[i] + currPoint[0];
-                int yy = dy[i] + currPoint[1];
-                if(xx >= 0 && xx < n && yy >= 0 && yy < m){
-                    if(maps[xx][yy] == 1 && !visited[xx][yy]){
-                        visited[xx][yy] = true;
-                        queue.add(new int[] {xx, yy, currPoint[2]+1});
-                    }
-                }
-
-            }
-        }
-        return -1;
-    }
-
-     */
 }
