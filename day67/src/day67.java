e2aac0038156f5201a67cd2fa472c664facbf527
chlpaegi01 <chlpaegi01@naver.com>
2024-01-23 13:00:49 +0900

    2024-01-23 13:00
    - 카운트다운

--- day67/src/day67.java
+++ day67/src/day67.java
@@ -1,5 +1,14 @@
 public class day67 {
+    /*
+        카운트다운
+        https://school.programmers.co.kr/learn/courses/30/lessons/131129
+     */
     public static void main(String[] args) {
-        
+
+    }
+
+    public int[] solution(int target) {
+        int[] answer = {};
+        return answer;
     }
 }
