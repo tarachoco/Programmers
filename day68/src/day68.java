1c855e5d1b2d807578e52ccbb67ecaf0a174c07d
chlpaegi01 <chlpaegi01@naver.com>
2024-02-01 08:55:58 +0900

    2024-02-01 08:55
    - day58

---
+++ day68/src/day68.java
@@ -0,0 +1,14 @@
+public class day68 {
+    /*
+        순위
+        https://school.programmers.co.kr/learn/courses/30/lessons/49191
+     */
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int n, int[][] results) {
+        int answer = 0;
+        return answer;
+    }
+}
