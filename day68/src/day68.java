6eebac8797021e6fb6f1654232f51e8c12fbec84
chlpaegi01 <chlpaegi01@naver.com>
2024-02-04 22:33:16 +0900

    2024-02-04 22:32
    - 68

--- day68/src/day68.java
+++ day68/src/day68.java
@@ -6,22 +6,55 @@ public class day68 {
         https://school.programmers.co.kr/learn/courses/30/lessons/49191
      */
     public static void main(String[] args) {
+        int[][] results = {{4, 3}, {4, 2}, {3, 2}, {1, 2}, {2, 5}};
+        int n = 5;
 
+        System.out.printf(""+solution(n, results));
     }
 
-    static int[][] graph;
-
-    public int solution(int n, int[][] results) {
+    public static int solution(int n, int[][] results) {
         int answer = 0;
-        
-        graph = new int[n+1][n+1];
-        Arrays.fill(graph, Integer.MAX_VALUE);
+        boolean[][] graph = new boolean[n+1][n+1];
 
-        // 플로이드 와샬 수행
-        floyd(n);
-        
+        for (int[] result : results) {
+            graph[result[0]][result[1]] = true;
+        }
+
+        // 경유지
+		for (int k = 1; k <= n; k++) {
+			// 출발지
+			for (int i = 1; i <= n; i++) {
+				//도착지
+				for (int j = 1; j <= n; j++) {
+					if (graph[i][k] && graph[k][j]) {
+                        graph[i][j] = true;
+                    }
+				}
+			}
+		}
+
+        for (int i=1; i<graph.length; i++) {
+            for (int j=1; j<graph[0].length; j++) {
+                if (graph[i][j] && graph[j][i]) {
+                    answer++;
+                }
+            }
+        }
+
+//        graph = new int[n+1][n+1];
+//        for (int i=0; i<graph.length; i++) {
+//            Arrays.fill(graph[i], Integer.MAX_VALUE);
+//        }
+//
+//        // 플로이드 와샬 수행
+//        floyd(n);
+//
         return answer;
     }
+
+
+
+//    static int[][] graph;
 
     /*
         k: 경유지
@@ -33,20 +66,20 @@ public class day68 {
 
         O^3 시간 복잡도지만 모든 경로를 다 계산할 수 있음.
      */
-    public int floyd (int n) {
-        int result = 0;
-
-        // 경유지
-		for (int k = 1; k <= n; k++) {
-			// 출발지
-			for (int i = 1; i <= n; i++) {
-				//도착지
-				for (int j = 1; j <= n; j++) {
-					graph[i][j] = Math.min(graph[i][j], graph[i][k]+graph[k][j]);
-				}
-			}
-		}
-
-        return result;
-    }
+//    public static int floyd (int n) {
+//        int result = 0;
+//
+//        // 경유지
+//		for (int k = 1; k <= n; k++) {
+//			// 출발지
+//			for (int i = 1; i <= n; i++) {
+//				//도착지
+//				for (int j = 1; j <= n; j++) {
+//					graph[i][j] = Math.min(graph[i][j], graph[i][k]+graph[k][j]);
+//				}
+//			}
+//		}
+//
+//        return result;
+//    }
 }
