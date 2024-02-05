863c65c14ba0884afcb3e9168a9527e26140e717
chlpaegi01 <chlpaegi01@naver.com>
2024-02-05 22:00:58 +0900

    2024-02-05 22:00
    데이68

--- day68/src/day68.java
+++ day68/src/day68.java
@@ -26,6 +26,7 @@ public class day68 {
 			for (int i = 1; i <= n; i++) {
 				//도착지
 				for (int j = 1; j <= n; j++) {
+                    // boolean type
 					if (graph[i][k] && graph[k][j]) {
                         graph[i][j] = true;
                     }
