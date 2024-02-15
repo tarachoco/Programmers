309a302f1afb656ff6d724a1cde51ce4fb2481a1
chlpaegi01 <chlpaegi01@naver.com>
2024-02-15 18:29:14 +0900

    2024-02-15 18:29
    - day70

--- day70/src/day70.java
+++ day70/src/day70.java
@@ -1,5 +1,14 @@
 public class day70 {
+    /*
+        여행 경로
+        https://school.programmers.co.kr/learn/courses/30/lessons/43164
+     */
     public static void main(String[] args) {
-        
+
+    }
+
+    public String[] solution(String[][] tickets) {
+        String[] answer = {};
+        return answer;
     }
 }
