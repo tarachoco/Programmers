75f5bcaa5bfd028af9b6993eb2d8a2e726425aa9
chlpaegi01 <chlpaegi01@naver.com>
2024-02-22 11:43:08 +0900

    2024-02-22 11:43
    - 야근 지수

--- day71/src/day71.java
+++ day71/src/day71.java
@@ -1,5 +1,14 @@
 public class day71 {
+    /*
+        야근 지수
+        https://school.programmers.co.kr/learn/courses/30/lessons/12927
+     */
     public static void main(String[] args) {
-        
+
+    }
+
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        return answer;
     }
 }
