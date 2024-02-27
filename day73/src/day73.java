6ad2ff1136277f864b78416de2b2e91e7ec83603
chlpaegi01 <chlpaegi01@naver.com>
2024-02-27 14:18:23 +0900

    2024-02-27 14:18
    - 등굣길

--- day73/src/day73.java
+++ day73/src/day73.java
@@ -1,5 +1,14 @@
 public class day73 {
+    /*
+        등굣길
+        https://school.programmers.co.kr/learn/courses/30/lessons/42898
+     */
     public static void main(String[] args) {
 
+    }
+
+    public int solution(int m, int n, int[][] puddles) {
+        int answer = 0;
+        return answer;
     }
 }
