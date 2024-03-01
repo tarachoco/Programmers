cc73fc5643db64b529f00f450b98eea7d96e2cc0
chlpaegi01 <chlpaegi01@naver.com>
2024-03-01 22:47:17 +0900

    2024-03-01 10:47
    - day73

--- day73/src/day73.java
+++ day73/src/day73.java
@@ -47,12 +47,13 @@ public class day73 {
     /*
         public int solution(int m, int n, int[][] puddles) {
         int mod = 1000000007;
-        
+
         int[][] board = new int[n + 1][m + 1];
         for(int i = 0; i < puddles.length; i++) {
-            board[puddles[i][1]][puddles[i][0]] = -1; 
+            board[puddles[i][1]][puddles[i][0]] = -1;
         }
-        
+
+        // dp 풀이
         board[1][1] = 1;
         for(int i = 1; i < n + 1; i++) {
             for(int j = 1; j < m + 1; j++) {
