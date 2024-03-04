23096acc96a9cc3f9365e0bb150587f46c7612cb
chlpaegi01 <chlpaegi01@naver.com>
2024-03-04 17:50:35 +0900

    2024-03-04 17:50
    - dp 풀이

--- day73/src/day73.java
+++ day73/src/day73.java
@@ -19,50 +19,49 @@ public class day73 {
     public static int solution(int m, int n, int[][] puddles) {
         min = Integer.MAX_VALUE;
         path = new HashMap<>();
-        map = new int[m+1][n+1];
+        map = new int[m+2][n+2];
 
         for (int[] puddle : puddles) {
             map[puddle[0]][puddle[1]] = -1;
         }
 
-        dfs(1, 1, 0);
-
-        return path.get(min);
-    }
-
-    public static void dfs(int x, int y, int count) {
-        if ( x+1 < map.length && map[x+1][y] > -1) {
-            dfs (x+1, y,count+1 );
-        }
-        if( y+1 < map[0].length && map[x][y+1] > -1 ) {
-            dfs (x, y+1,count+1 );
-        }
-
-        if ( x+1 == map.length && y+1 == map[0].length) {
-            min = Math.min(min, count);
-            path.put(count, path.getOrDefault(count, 0)+1);
-        }
-    }
-
-    /*
-        public int solution(int m, int n, int[][] puddles) {
-        int mod = 1000000007;
-
-        int[][] board = new int[n + 1][m + 1];
-        for(int i = 0; i < puddles.length; i++) {
-            board[puddles[i][1]][puddles[i][0]] = -1;
-        }
-
         // dp 풀이
-        board[1][1] = 1;
-        for(int i = 1; i < n + 1; i++) {
-            for(int j = 1; j < m + 1; j++) {
-                if(board[i][j] == -1) continue;
-                if(board[i - 1][j] > 0) board[i][j] += board[i - 1][j] % mod;
-                if(board[i][j - 1] > 0) board[i][j] += board[i][j - 1] % mod;
+        map[1][1] = 1;
+        for(int i=1; i<=m; i++) {
+            for(int j=1; j<=n; j++) {
+                if (map[i][j] == -1) {
+                    continue;
+                }
+                if (map[i][j-1] > 0) {
+                    map[i][j] += map[i][j-1] % 1000000007;
+                }
+                if (map[i-1][j] > 0) {
+                    map[i][j] += map[i-1][j] % 1000000007;
+                }
             }
         }
-        return board[n][m] % mod;
+
+        return map[m][n] % 1000000007;
+
+        // dfs 효율성 테스트 실패
+//        dfs(1, 1, 0);
+//        return path.get(min);
     }
-    */
+//
+//    public static void dfs(int x, int y, int count) {
+//
+//        count %= 1000000007;
+//
+//        if ( x+1 < map.length && map[x+1][y] > -1) {
+//            dfs (x+1, y,count+1 );
+//        }
+//        if( y+1 < map[0].length && map[x][y+1] > -1 ) {
+//            dfs (x, y+1,count+1 );
+//        }
+//
+//        if ( x+1 == map.length && y+1 == map[0].length) {
+//            min = Math.min(min, count);
+//            path.put(count, path.getOrDefault(count, 0)+1);
+//        }
+//    }
 }
