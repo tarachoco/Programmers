b505fe3f97c44ab74a19980fe5e5cf767f3ff0a1
chlpaegi01 <chlpaegi01@naver.com>
2024-03-05 18:11:37 +0900

    2024-03-05 18:11
    - day74

--- day74/src/day74.java
+++ day74/src/day74.java
@@ -1,5 +1,14 @@
 public class day74 {
+    /*
+        단속카메라
+        https://school.programmers.co.kr/learn/courses/30/lessons/42884
+     */
     public static void main(String[] args) {
         
+    }
+
+    public int solution(int[][] routes) {
+        int answer = 0;
+        return answer;
     }
 }
