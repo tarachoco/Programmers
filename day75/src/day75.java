74410b4450c5102a21a974e502c076cb49679395
chlpaegi01 <chlpaegi01@naver.com>
2024-03-08 10:34:32 +0900

    2024-03-08 10:34
    - day75

--- day75/src/day75.java
+++ day75/src/day75.java
@@ -4,10 +4,11 @@ public class day75 {
         https://school.programmers.co.kr/learn/courses/30/lessons/42627
      */
     public static void main(String[] args) {
-
+        int[][] jobs = {{0, 3}, {1, 9}, {2, 6}};
+        System.out.printf(""+solution(jobs));
     }
 
-    public int solution(int[][] jobs) {
+    public static int solution(int[][] jobs) {
         int answer = 0;
         return answer;
     }
