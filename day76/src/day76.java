a8ac0ff2200ba6667cf3c5005f81f44bc96757b2
chlpaegi01 <chlpaegi01@naver.com>
2024-03-14 13:46:36 +0900

    2024-03-14 13:46
    - day76

--- day76/src/day76.java
+++ day76/src/day76.java
@@ -1,5 +1,14 @@
 public class day76 {
+    /*
+        선입 선출 스케줄링
+        https://school.programmers.co.kr/learn/courses/30/lessons/12920
+     */
     public static void main(String[] args) {
-        
+
+    }
+
+    public int solution(int n, int[] cores) {
+        int answer = 0;
+        return answer;
     }
 }
