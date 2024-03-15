8e9c44d65a4391be4c7cb5f2235bfd704b9f224b
chlpaegi01 <chlpaegi01@naver.com>
2024-03-15 14:33:31 +0900

    2024-03-15 14:33
    - day76

--- day76/src/day76.java
+++ day76/src/day76.java
@@ -4,11 +4,40 @@ public class day76 {
         https://school.programmers.co.kr/learn/courses/30/lessons/12920
      */
     public static void main(String[] args) {
-
+        int n = 6;
+        int[] cores = {1, 2, 3};
+        System.out.printf("" + solution(n, cores));
     }
 
-    public int solution(int n, int[] cores) {
+    public static int solution(int n, int[] cores) {
         int answer = 0;
+
         return answer;
     }
+
+    /*
+        정확성은 성공이나 효율성은 낮음.
+        이분탐색으로 시간복잡도 단축 필요 
+     */
+//    public static int solution(int n, int[] cores) {
+//        int answer = 0;
+//        int index = cores.length;
+//        int step = 0;
+//
+//        while (index < n) {
+//            step++;
+//
+//            for (int i=0; i<cores.length; i++) {
+//                if (step % cores[i] == 0) {
+//                    index++;
+//                    if (index == n) {
+//                        answer = i+1;
+//                        break;
+//                    }
+//                }
+//            }
+//        }
+//
+//        return answer;
+//    }
 }
