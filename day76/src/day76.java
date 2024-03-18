026679d1e27d0f38d5569735884b69aed5ee194c
chlpaegi01 <chlpaegi01@naver.com>
2024-03-18 16:28:46 +0900

    2024-03-18 16:28
    - 선입 선출 스케줄링

--- day76/src/day76.java
+++ day76/src/day76.java
@@ -11,8 +11,59 @@ public class day76 {
 
     public static int solution(int n, int[] cores) {
         int answer = 0;
+        int min = 0;
+        int max = 10000*n;
+
+        int time = 0;
+        int count = 0;
+
+        // 이분탐색을 통해 마지막 작업을 수행하는 시간을 구한다.
+        while (true) {
+            int mid = (min+max)/2;
+
+            count = calculate(mid, cores);
+
+            if (min > max) {
+                break;
+            }
+
+            // 작업 시간 갱신
+            if (count >= n) { 
+                max = mid-1;
+                time = mid;
+            } else {
+                min = mid+1;
+            }
+        }
+
+        count-=n;
+        for (int i = cores.length-1; i>=0; i--){
+            if (time % cores[i] == 0) {
+                if (count == 0) {
+                    answer = i+1;
+                    break;
+                }
+                count--;
+            }
+        }
 
         return answer;
+    }
+
+    public static int calculate (int time, int[] cores) {
+
+        if (time == 0) {
+            return cores.length;
+        }
+
+        int count = cores.length; 
+        
+        // time까지 코어가 처리 가능한 작업 개수 갱신
+        for(int i = 0; i<cores.length; i++){
+            count += (time/cores[i]);
+        }
+
+        return count;
     }
 
     /*
