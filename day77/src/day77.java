b2c20b86485f2b1445685645a8cf053effc2a85f
chlpaegi01 <chlpaegi01@naver.com>
2024-03-22 16:05:13 +0900

    2024-03-22 16:05
    - day77

--- day77/src/day77.java
+++ day77/src/day77.java
@@ -4,7 +4,9 @@ public class day77 {
         https://school.programmers.co.kr/learn/courses/30/lessons/43163
     */
     public static void main(String[] args) {
-        
+        String begin = "hit";
+        String target = "cog";
+        String[] words = {"hot", "dot", "dog", "lot", "log", "cog"};
     }
 
     public int solution(String begin, String target, String[] words) {
