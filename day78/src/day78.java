094f9e7972e174cd5d0a91fd1d02e8152e87a1e0
chlpaegi01 <chlpaegi01@naver.com>
2024-03-29 18:26:51 +0900

    2024-03-29 18:26
    - 섬 연결하기

--- day78/src/day78.java
+++ day78/src/day78.java
@@ -1,4 +1,8 @@
 public class day78 {
+    /*
+        섬 연결하기
+        https://school.programmers.co.kr/learn/courses/30/lessons/42861
+     */
     public static void main(String[] args) {
         
     }
