555b2328b7cb8bd491025e1b9bd9af3dd2f4df98
chlpaegi01 <chlpaegi01@naver.com>
2024-04-08 11:02:51 +0900

    2024-04-08 11:02
    - 주석처리

--- day79/src/day79.java
+++ day79/src/day79.java
@@ -22,46 +22,51 @@ public class day79 {
      */
     public static int solution(String arr[]) {
         int answer = -1;
-
-        String arr2[] = arr.clone();
-
-        for (int i=0; i<arr.length-1; i=i+2) {
-            int a1 = apply(arr[i], arr[i+1], arr[i+2]);
-            int a2 = i >= arr.length - 3 ? Integer.MIN_VALUE : apply(arr[i], arr[i+1], String.valueOf(apply(arr[i+2], arr[i+3], arr[i+4])));
-
-            if (a1 > a2) {
-                arr[i+2] = String.valueOf(a1);
-            } else {
-                arr[i+4] = String.valueOf(a2);
-                i+=2;
-            }
-        }
-
-        for (int i=arr2.length-1; i>1; i=i-2) {
-            int a1 = apply(arr2[i-2], arr2[i-1], arr2[i]);
-            int a2 = i < 3 ? Integer.MIN_VALUE : apply(String.valueOf(apply(arr2[i-2], arr2[i-3], arr2[i-4])), arr2[i-1], arr2[i] );
-
-            if (a1 > a2) {
-                arr2[i-2] = String.valueOf(a1);
-            } else {
-                arr2[i-4] = String.valueOf(a2);
-                i-=2;
-            }
-        }
-
-        answer = Math.max(Integer.parseInt(arr[arr.length-1]),  Integer.parseInt(arr2[0]));
-
         return answer;
     }
 
-    public static int apply (String start, String oper, String end) {
-        int a1 = Integer.parseInt(start);
-        int a2 = Integer.parseInt(end);
-
-        if ("+".equals(oper)) {
-            return a1 + a2;
-        } else {
-            return a1 - a2;
-        }
-    }
+//    public static int solution(String arr[]) {
+//        int answer = -1;
+//
+//        String arr2[] = arr.clone();
+//
+//        for (int i=0; i<arr.length-1; i=i+2) {
+//            int a1 = apply(arr[i], arr[i+1], arr[i+2]);
+//            int a2 = i >= arr.length - 3 ? Integer.MIN_VALUE : apply(arr[i], arr[i+1], String.valueOf(apply(arr[i+2], arr[i+3], arr[i+4])));
+//
+//            if (a1 > a2) {
+//                arr[i+2] = String.valueOf(a1);
+//            } else {
+//                arr[i+4] = String.valueOf(a2);
+//                i+=2;
+//            }
+//        }
+//
+//        for (int i=arr2.length-1; i>1; i=i-2) {
+//            int a1 = apply(arr2[i-2], arr2[i-1], arr2[i]);
+//            int a2 = i < 3 ? Integer.MIN_VALUE : apply(String.valueOf(apply(arr2[i-2], arr2[i-3], arr2[i-4])), arr2[i-1], arr2[i] );
+//
+//            if (a1 > a2) {
+//                arr2[i-2] = String.valueOf(a1);
+//            } else {
+//                arr2[i-4] = String.valueOf(a2);
+//                i-=2;
+//            }
+//        }
+//
+//        answer = Math.max(Integer.parseInt(arr[arr.length-1]),  Integer.parseInt(arr2[0]));
+//
+//        return answer;
+//    }
+//
+//    public static int apply (String start, String oper, String end) {
+//        int a1 = Integer.parseInt(start);
+//        int a2 = Integer.parseInt(end);
+//
+//        if ("+".equals(oper)) {
+//            return a1 + a2;
+//        } else {
+//            return a1 - a2;
+//        }
+//    }
 }
