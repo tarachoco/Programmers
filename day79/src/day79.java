7716853ff62ccf9fb601de206f4939501eb34e89
chlpaegi01 <chlpaegi01@naver.com>
2024-04-09 15:49:07 +0900

    2024-04-09 15:49
    - day79

--- day79/src/day79.java
+++ day79/src/day79.java
@@ -20,9 +20,70 @@ public class day79 {
         a+b 의 최솟값 -> a는 최솟값, b도 최솟값
         a-b 의 최솟값 -> a는 최솟값, b는 최댓값
      */
+    public static int[][][] dp;
+    public static int numbers[];
+    public static String operations[];
+
     public static int solution(String arr[]) {
         int answer = -1;
+        int size = arr.length/2+1;
+
+        // dp 초기화
+        dp = new int[2][size][size];
+        numbers = new int[size];
+        operations = new String[size-1];
+
+        int n1 = 0;
+        int o1 = 0;
+
+        for (int i=0; i<arr.length; i++) {
+            if (i/2 == 0) {
+                numbers[n1++] = Integer.parseInt(arr[i]);
+            } else {
+                operations[o1++] = arr[i];
+            }
+        }
+
+
         return answer;
+    }
+
+    /*
+        a+b -> a=max, b=max
+        a-b -> a=max, b=min
+        -(a+b) -> a=min, b=min
+        -(a-b) -> a=min, b=max
+
+        operation
+            0:minus : min
+            1:plus : max
+     */
+    public static int calculate(int operation, int start, int end) {
+
+        // 마지막 숫자 하나가 선택된 경우
+        if (start == end) {
+            dp[operation][start][end] = numbers[start];
+            return dp[operation][start][end];
+        }
+
+        int temp = (operation == 0) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+
+        switch (operation) {
+            case 0: // minus : 최소를 구해야함
+                for (int s = start; s<end; s++) {
+                    if ("+".equals(operations[s])) {
+
+                    }
+                }
+                break;
+            case 1: // plus : 최대를 구해야함
+
+                break;
+        }
+
+        dp[operation][start][end] = temp;
+
+        return 0;
     }
 
 //    public static int solution(String arr[]) {
