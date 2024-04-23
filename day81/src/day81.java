579937dc1a840d72a1846da23ce96da55209e361
chlpaegi01 <chlpaegi01@naver.com>
2024-04-24 08:28:22 +0900

    2024-04-24 08:28
    - day81

---
+++ day81/src/day81.java
@@ -0,0 +1,15 @@
+public class day81 {
+    /*
+        마법의 엘리베이터
+        https://school.programmers.co.kr/learn/courses/30/lessons/148653
+     */
+    public static void main(String[] args) {
+        int storey = 16;
+        System.out.printf(""+solution(storey));
+    }
+
+    public static int solution(int storey) {
+        int answer = 0;
+        return answer;
+    }
+}
