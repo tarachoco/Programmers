9d106b643cab99a27f801b3b6a49d79d74f8389c
tarachoco <104431722+tarachoco@users.noreply.github.com>
2024-05-06 12:00:41 +0900

    Update day83.java

--- day83/src/day83.java
+++ day83/src/day83.java
@@ -1,5 +1,14 @@
 public class day83 {
+    /*
+        거스름돈
+        https://school.programmers.co.kr/learn/courses/30/lessons/12907
+    */
     public static void main(String[] args) {
 
+    }
+
+    public static int solution(int n, int[] money) {
+        int answer = 0;
+        return answer;
     }
 }
