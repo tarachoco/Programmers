57de1354f65f9364f5b7fad18712f8e5dc33a9c2
chlpaegi01 <chlpaegi01@naver.com>
2024-05-16 07:51:26 +0900

    2024-05-16 07:51
    -day85

--- day85/src/day85.java
+++ day85/src/day85.java
@@ -1,5 +1,14 @@
 public class day85 {
+    /*
+        보석 쇼핑
+        https://school.programmers.co.kr/learn/courses/30/lessons/67258
+     */
     public static void main(String[] args) {
 
+    }
+
+    public static int[] solution(String[] gems) {
+        int[] answer = {};
+        return answer;
     }
 }
