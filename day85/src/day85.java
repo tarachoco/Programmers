e795a5207b9832ac4f88d5a206cfaf7f30f8f912
chlpaegi01 <chlpaegi01@naver.com>
2024-05-17 10:28:24 +0900

    2024-05-17 10:28
    - 보석쇼핑

--- day85/src/day85.java
+++ day85/src/day85.java
@@ -1,14 +1,59 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class day85 {
     /*
         보석 쇼핑
         https://school.programmers.co.kr/learn/courses/30/lessons/67258
      */
     public static void main(String[] args) {
-
+        String[] gems = {"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"};
+//        String[] gems = {"AA", "AB", "AC", "AA", "AC"};
+//        String[] gems = {"XYZ", "XYZ", "XYZ"};
+//        String[] gems = {"ZZZ", "YYY", "NNNN", "YYY", "BBB"};
+//        String[] gems = {"test"};
+        int[] answer = solution(gems);
+        System.out.printf(answer[0]+","+answer[1]);
     }
 
+    /*
+        투포인터 문제
+        첫번째 보석이 2개 이상일 경우 구간 첫번째 보석을 제거한다.
+     */
     public static int[] solution(String[] gems) {
-        int[] answer = {};
+        int[] answer = new int[2];
+
+        // #1 중복 제거한 보석 종류 구하기
+        Set<String> kinds = Arrays.stream(gems).collect(Collectors.toSet());
+        Set<String> temp = new HashSet<>();
+
+        int start = 0;
+        int end = 0;
+        int range = Integer.MAX_VALUE;
+
+        while (end < gems.length) {
+
+            temp.add(gems[end]);
+
+            if (temp.size() == kinds.size()) {
+                if (range > (end - start)) {
+                    range = end - start;
+                    answer[0] = start+1;
+                    answer[1] = end+1;
+                }
+            }
+
+            if (end > start && gems[end] == gems[start]) {
+                start++;
+                end = start+1;
+                temp.clear();
+                temp.add(gems[start]);
+            } else {
+                end++;
+            }
+
+        }
+
         return answer;
     }
 }
