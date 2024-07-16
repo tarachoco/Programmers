5982984857a8b65927cd31746a061758c12d1cf4
chlpaegi01 <chlpaegi01@naver.com>
2024-07-17 08:01:30 +0900

    2024-07-17 08:01
    - 문자열을 정수로 바꾸기

--- day116/src/day116.java
+++ day116/src/day116.java
@@ -1,2 +1,13 @@
 public class day116 {
+    /*
+        문자열을 정수로 바꾸기
+        https://school.programmers.co.kr/learn/courses/30/lessons/12925
+     */
+    public static void main(String[] args) {
+        System.out.printf(""+solution("-1234"));
+    }
+
+    public static int solution(String s) {
+        return Integer.parseInt(s);
+    }
 }
