c43060540160df39680145f4761b16104a8aac5f
chlpaegi01 <chlpaegi01@naver.com>
2024-07-26 17:48:02 +0900

    자동 커밋 메시지: 2024-07-26 17:48:02.45

--- auto_commit.bat
+++ auto_commit.bat
@@ -1,7 +1,7 @@
 @echo off
 chcp 65001 >nul
 REM 스크립트 시작
-setlocal
+setlocal enabledelayedexpansion
 
 REM 현재 디렉터리를 저장합니다.
 set CURRENT_DIR=%cd%
@@ -9,20 +9,32 @@ set CURRENT_DIR=%cd%
 REM Git 저장소 디렉터리로 이동합니다.
 cd /d "D:\STUDY\Programmers"
 
-REM Git 상태를 확인합니다.
-git status
+REM Git 상태를 확인하고 변경된 파일 목록을 얻습니다.
+for /f "tokens=2* delims= " %%i in ('git status --porcelain') do (
+    set FIRST_FILE=%%i
+    goto :found
+)
+:found
 
-REM 모든 변경 사항을 추가합니다.
-git add .
+REM 변경된 파일이 있는 경우 추가합니다.
+if defined FIRST_FILE (
+    REM 첫 번째 파일을 추가합니다.
+    echo git add "!FIRST_FILE!"
+    git add "!FIRST_FILE!"
 
-REM 커밋 메시지를 설정합니다.
-set COMMIT_MSG="자동 커밋 메시지: %date% %time%"
+    REM 커밋 메시지를 설정합니다.
+    set COMMIT_MSG=자동 커밋 메시지: %date% %time%
 
-REM 커밋을 수행합니다.
-git commit -m %COMMIT_MSG%
+    REM 커밋을 수행합니다.
+    echo git commit -m "!COMMIT_MSG!"
+    git commit -m "!COMMIT_MSG!"
 
-REM 원격 저장소에 푸시합니다.
-git push origin master
+    REM 원격 저장소에 푸시합니다.
+    echo git push origin master
+    git push origin master
+) else (
+    echo 변경된 파일이 없습니다.
+)
 
 REM 원래 디렉터리로 돌아갑니다.
 cd /d %CURRENT_DIR%
