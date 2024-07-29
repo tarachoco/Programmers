b6740b79f41e487c1313d456fe0c9c395a500788
chlpaegi01 <chlpaegi01@naver.com>
2024-07-29 17:37:00 +0900

    자동 커밋 메시지: 2024-07-29 17:36:36.62

--- day124/src/day124.java
+++ day124/src/day124.java
@@ -1,2 +1,21 @@
 public class day124 {
+    /*
+        약수의 합
+        https://school.programmers.co.kr/learn/courses/30/lessons/12928
+     */
+    public static void main(String[] args) {
+        System.out.printf(""+solution(12));
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+
+        for (int i=1; i<=n; i++) {
+            if (n % i == 0) {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
 }
